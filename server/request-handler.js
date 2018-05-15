8d249396a5496af9373de2024f9577144169c12c
unfaceit <ryabtsev.art@gmail.com>
2018-05-14 19:06:52 -0700

    Completed Implementation of Get and Post. Passing all tests.

--- server/request-handler.js
+++ server/request-handler.js
@@ -23,45 +23,12 @@ let results = {
 };
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-
-
-
-  // The outgoing status.
   let statusCode = 200;
   let headers = defaultCorsHeaders;
   headers['Content-Type'] = 'application/json';
-
-  // See the note below about CORS headers.
-  // var headers = defaultCorsHeaders;
-  // let headers = defaultCorsHeaders;
-  // headers = defaultCorsHeaders;
-
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  //'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // response.writeHead(response.statusCode, headers);
 
   if (request.url !== '/classes/messages') {
     statusCode = 404;
@@ -75,11 +42,6 @@ var requestHandler = function(request, response) {
   }
 
   if (request.method === 'GET') {
-    // if (!request.url.includes('/classes/messages')) {
-    //   statusCode = 404;
-    //   response.writeHead(statusCode, headers);
-    //   response.end();
-    // } else {
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify(results));
   }
@@ -89,38 +51,14 @@ var requestHandler = function(request, response) {
 
     let mailbox = [];
     request.on('data', (data) => {
-      console.log('we \'re in post', data);
       mailbox.push(data);
     }).on('end', () => {
       mailbox = Buffer.concat(mailbox).toString();
       results.results.push(JSON.parse(mailbox));
-      response.writeHead(statusCode, headers);
-      console.log('we \'re in end', results);
     });
+    response.writeHead(statusCode, headers);
     response.end(JSON.stringify(results));
   }
-
-
-
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  // response.end('Hello, World!');
 };
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
 exports.requestHandler = requestHandler;
